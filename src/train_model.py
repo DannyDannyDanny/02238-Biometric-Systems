fda83e360a51affe62825aad516d7ed032070a9e
DannyDannyDanny <s144222@student.dtu.dk>
2019-06-26 20:13:50 +0200

    wip: cleanup imports

--- src/train_model.py
+++ src/train_model.py
@@ -2,17 +2,14 @@ from __future__ import print_function
 import numpy as np
 import pandas as pd
 from PIL import Image
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
 import keras
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from keras.optimizers import SGD
 from IPython.display import display
 import os
 import glob
