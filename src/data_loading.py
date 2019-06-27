d7492290e431a47b88c74a1f8587d85bdaa49c7c
DannyDannyDanny <s144222@student.dtu.dk>
2019-06-27 17:54:32 +0200

    feat: load data in a function

--- src/data_loading.py
+++ src/data_loading.py
@@ -12,51 +12,23 @@ import glob
 import numpy as np
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
-# from keras.preprocessing.image import array_to_img
-# img_pil = array_to_img(img_array)
-# img_pil.show()
 
 # %% dataprep
-train_files = list(sorted(glob.glob('./data/THU-DorsalFinger/FDT2_Train/*/*.bmp')))
-test_files = list(sorted(glob.glob('./data/THU-DorsalFinger/FDT2_Test/*/*.bmp')))
+def get_pairs():
+    train_files = list(sorted(glob.glob('./data/THU-DorsalFinger/FDT2_Train/*/*.bmp')))
+    test_files = list(sorted(glob.glob('./data/THU-DorsalFinger/FDT2_Test/*/*.bmp')))
 
-train_hash = hash(''.join([f[-7:] for f in train_files]))
-test_hash = hash(''.join([f[-7:] for f in test_files]))
-assert test_hash == train_hash
+    train_hash = hash(''.join([f[-7:] for f in train_files]))
+    test_hash = hash(''.join([f[-7:] for f in test_files]))
+    assert test_hash == train_hash
 
-pairs = []
+    pairs = []
 
-for file_train,file_test in zip(train_files,test_files):
-    subject_id = int(file_test.split('/')[-2])
-    img_train = load_img(file_train,color_mode = "grayscale")
-    arr_train = img_to_array(img_train)
-    img_test = load_img(file_test,color_mode = "grayscale")
-    arr_test = img_to_array(img_test)
-    pair.append((arr_test,arr_train))
-
-# %% ---- ABOVE BEEN COPIED
-
-set([i.shape for i in x_train])
-
-batch_size = 32
-num_classes = 10
-epochs = 100
-data_augmentation = True
-num_predictions = 20
-save_dir = os.path.join(os.getcwd(), 'saved_models')
-model_name = 'keras_cifar10_trained_model.h5'
-
-(x_train1, y_train1), (x_test1, y_test1) = cifar10.load_data()
-type(x_train1)
-x_train1.shape
-x_train[0].shape
-#model.add(Conv2D(32, (3, 3), padding='same',input_shape=x_train.shape[1:]))
-
-type(x_train)
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# Convert class vectors to binary class matrices.
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+    for file_train,file_test in zip(train_files,test_files):
+        subject_id = int(file_test.split('/')[-2])
+        img_train = load_img(file_train,color_mode = "grayscale")
+        arr_train = img_to_array(img_train)
+        img_test = load_img(file_test,color_mode = "grayscale")
+        arr_test = img_to_array(img_test)
+        pair.append((arr_test,arr_train))
+    return pairs
