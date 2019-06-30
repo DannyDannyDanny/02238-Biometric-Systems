52f2c08fdeb67c2f5cf9ebdd4ad7c4970872b300
DannyDannyDanny <s144222@student.dtu.dk>
2019-06-30 21:02:41 +0200

    feat: squeeze option add to data loading for image_array shape (x, y, 1)

--- src/data_loading.py
+++ src/data_loading.py
@@ -17,7 +17,7 @@ from skimage import exposure
 
 
 # %% dataprep
-def get_pairs():
+def get_pairs(squeeze=False):
     train_files = list(sorted(glob.glob('./data/THU-DorsalFinger/FDT2_Train/*/*.bmp')))
     test_files = list(sorted(glob.glob('./data/THU-DorsalFinger/FDT2_Test/*/*.bmp')))
 
@@ -32,20 +32,19 @@ def get_pairs():
 
         img_train = load_img(file_train,color_mode = "grayscale")
         arr_train = exposure.equalize_hist(img_to_array(img_train))
-        arr_train = np.squeeze((img_to_array(arr_train)))
+        if squeeze: arr_train = np.squeeze(arr_train)
 
         img_test = load_img(file_test,color_mode = "grayscale")
         arr_test = exposure.equalize_hist(img_to_array(img_test))
-        arr_test = np.squeeze((img_to_array(arr_test)))
+        if squeeze: arr_test = np.squeeze(arr_test)
 
         pairs.append([arr_test,arr_train])
     return pairs
 
 # %%
-all_pairs = np.array(get_pairs())
 
-def get_x_y():
-    all_pairs = np.array(get_pairs())
+def get_x_y(ttr=0.2,squeeze=False,v=0):
+    all_pairs = np.array(get_pairs(squeeze=squeeze))
 
     # assert all images unique
     string_imgs = []
@@ -69,56 +68,70 @@ def get_x_y():
         r.shuffle(ran1)
         r.shuffle(ran2)
         no_matches = sum([a==b for (a,b) in zip(ran1,ran2)]) == 0
-        if no_matches:
-            print('Non-matching list generated')
-        else:
-            print('No matches:',no_matches)
+        if v >= 1:
+            if no_matches:
+                print('Non-matching list generated')
+            else:
+                print('No matches:',no_matches)
 
     nmatch = np.array([[rem[a][0],rem[b][1]] for (a,b) in zip(ran1,ran2)])
     ymatch = np.array(ymatch)
-
 
     # add class
     ymatch = [[1,i] for i in ymatch]
     nmatch = [[0,i] for i in nmatch]
     # merge and shuffle
-    class_pairs = ymatch + nmatch
-    r.shuffle(class_pairs)
+    ttr = 0.2
+    tts = int(len(ymatch)*ttr)
+    te_pairs = ymatch[tts:] + nmatch[tts:]
+    tr_pairs = ymatch[:tts] + nmatch[:tts]
+    r.shuffle(te_pairs)
+    r.shuffle(tr_pairs)
 
-    data_y = np.array([y for y,x in class_pairs])
-    # data_y = np.array([[y] for y,x in class_pairs])
-    data_x = np.array([x for y,x in class_pairs])
-    return data_x,data_y
+    te_x = np.array([x for y,x in te_pairs])
+    tr_x = np.array([x for y,x in tr_pairs])
+    te_y = np.array([y for y,x in te_pairs])
+    tr_y = np.array([y for y,x in tr_pairs])
 
+    return te_x, tr_x, te_y, tr_y
+    # return data_x,data_y
+
+# %%
+te_x, tr_x, te_y, tr_y = get_x_y(squeeze=True)
+
+ste_x, str_x, ste_y, str_y = get_x_y(squeeze=False)
+# %%
+te_x.shape
+te_x[0][0].shape
+# %%
+ste_x.shape
+ste_x[0][0].shape
+
+
+# %%
 all_pairs = get_pairs()
 img_origin_lookup_dict = {}
 for i,(a,b) in enumerate(all_pairs):
     img_origin_lookup_dict[b.tostring()] = (i,'b')
     img_origin_lookup_dict[a.tostring()] = (i,'a')
 
-
-pairs, y = get_x_y()
-
-
-pairs.shape
-y.shape
-
-match = 0
-nonmatch = 0
-errors = 0
-# %%
-for pair in pairs:
-    a = img_origin_lookup_dict[pair[0].tostring()]
-    b = img_origin_lookup_dict[pair[1].tostring()]
-    if a[0] == b[0]:
-        match += 1
-    else:
-        nonmatch += 1
-
-    if a[1] == b[1]:
-        errors += 1
-
-
-match
-nonmatch
-errors
+# # %%
+# match = 0
+# nonmatch = 0
+# errors = 0
+# # %%
+# for pair in pairs:
+#     a = img_origin_lookup_dict[pair[0].tostring()]
+#     b = img_origin_lookup_dict[pair[1].tostring()]
+#     if a[0] == b[0]:
+#         match += 1
+#     else:
+#         nonmatch += 1
+#
+#     if a[1] == b[1]:
+#         errors += 1
+#
+#
+# match
+# nonmatch
+# errors
