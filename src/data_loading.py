c5d508e42a8525aff8122b532b75ed0eaf7f3a86
DannyDannyDanny <s144222@student.dtu.dk>
2019-06-30 15:18:41 +0200

    feat: get_x_y function generates equal-class-split of shuffled pairs

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import random as r
 import keras
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
@@ -27,8 +28,44 @@ def get_pairs():
     for file_train,file_test in zip(train_files,test_files):
         subject_id = int(file_test.split('/')[-2])
         img_train = load_img(file_train,color_mode = "grayscale")
-        arr_train = img_to_array(img_train)
+        arr_train = np.squeeze((img_to_array(img_train)))
         img_test = load_img(file_test,color_mode = "grayscale")
-        arr_test = img_to_array(img_test)
-        pairs.append((arr_test,arr_train))
+        arr_test = np.squeeze((img_to_array(img_test)))
+        pairs.append([arr_test,arr_train])
     return pairs
+
+def get_x_y():
+    all_pairs = np.array(get_pairs())
+    half_len = len(all_pairs)//2
+    ymatch = all_pairs[:half_len]
+    rem = all_pairs[half_len:]
+    # generating class shuffled pairs
+    no_matches = False
+    while not no_matches:
+        ran1 = [i for i in range(half_len)]
+        ran2 = [i for i in range(half_len)]
+        r.shuffle(ran1)
+        r.shuffle(ran2)
+        no_matches = sum([a==b for (a,b) in zip(ran1,ran2)]) == 0
+        if no_matches:
+            print('Non-matching list generated')
+        else:
+            print('No matches:',no_matches)
+
+    nmatch = np.array([[rem[a][0],rem[a][1]] for (a,b) in zip(ran1,ran2)])
+    ymatch = np.array(ymatch)
+    nmatch.shape
+    ymatch.shape
+
+    # add class
+    ymatch = [[1,i] for i in ymatch]
+    nmatch = [[0,i] for i in nmatch]
+    # merge and shuffle
+    class_pairs = ymatch + nmatch
+    r.shuffle(class_pairs)
+
+    len(class_pairs)
+    class_pairs[0][0]
+    data_y = np.array([y for y,x in class_pairs])
+    data_x = np.array([x for y,x in class_pairs])
+    return data_x,data_y
