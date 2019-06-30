29aecc18de127dd1e7352081c7bc3678e67012ea
DannyDannyDanny <s144222@student.dtu.dk>
2019-06-30 15:49:39 +0200

    wip: Siamese-CNN

--- src/keras_mnist_one_shot.py
+++ src/keras_mnist_one_shot.py
@@ -1,6 +1,23 @@
 from __future__ import absolute_import
-from __future__ import print_function
 import numpy as np
+import pandas as pd
+from PIL import Image
+from keras.utils import np_utils
+from keras import backend as K
+import keras
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, BatchNormalization
+from keras.optimizers import SGD
+from IPython.display import display
+import os
+import glob
+import numpy as np
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.regularizers import l2
+
 
 import random
 from keras.datasets import mnist
@@ -8,6 +25,7 @@ from keras.models import Model
 from keras.layers import Input, Flatten, Dense, Dropout, Lambda
 from keras.optimizers import RMSprop
 from keras import backend as K
+from keras.models import Sequential
 
 num_classes = 10
 epochs = 20
@@ -53,17 +71,75 @@ def create_pairs(x, digit_indices):
     return np.array(pairs), np.array(labels)
 
 
-def create_base_network(input_shape):
+def create_base_network(input_shape,model_type='dim'):
     '''Base network to be shared (eq. to feature extraction).
     '''
-    input = Input(shape=input_shape)
-    x = Flatten()(input)
-    x = Dense(128, activation='relu')(x)
-    x = Dropout(0.1)(x)
-    x = Dense(128, activation='relu')(x)
-    x = Dropout(0.1)(x)
-    x = Dense(128, activation='relu')(x)
-    return Model(input, x)
+    print('-inputshape->',input_shape)
+    if model_type == 'alexnet':
+        print('using ALEXNET')
+        #Instantiate an empty model
+        model = Sequential()
+        # 1st Convolutional Layer
+        model.add(Conv2D(filters=96, input_shape=input_shape, kernel_size=(11,11), strides=(4,4), padding='valid'))
+        model.add(Activation('relu'))
+        # Max Pooling
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+        # 2nd Convolutional Layer
+        model.add(Conv2D(filters=256, kernel_size=(11,11), strides=(1,1), padding='valid'))
+        model.add(Activation('relu'))
+        # Max Pooling
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+        # 3rd Convolutional Layer
+        model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+        model.add(Activation('relu'))
+        # 4th Convolutional Layer
+        model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+        model.add(Activation('relu'))
+        # 5th Convolutional Layer
+        model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='valid'))
+        model.add(Activation('relu'))
+        # Max Pooling
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+        # Passing it to a Fully Connected layer
+        model.add(Flatten())
+        # 1st Fully Connected Layer
+        model.add(Dense(4096, input_shape=(224*224*3,)))
+        model.add(Activation('relu'))
+        # Add Dropout to prevent overfitting
+        model.add(Dropout(0.4))
+        # 2nd Fully Connected Layer
+        model.add(Dense(4096))
+        model.add(Activation('relu'))
+        # Add Dropout
+        model.add(Dropout(0.4))
+        # 3rd Fully Connected Layer
+        model.add(Dense(1000))
+        model.add(Activation('relu'))
+        # Add Dropout
+        model.add(Dropout(0.4))
+        # Output Layer
+        model.add(Dense(17))
+        model.add(Activation('softmax'))
+        x = Dense(128, activation='relu')(model)
+        return Model(x)
+    if model_type == 'dim':
+        input = Input(shape=input_shape)
+        x = Flatten()(input)
+        x = Dense(128, activation='relu')(x)
+        x = Dropout(0.1)(x)
+        x = Dense(128, activation='relu')(x)
+        x = Dropout(0.1)(x)
+        x = Dense(128, activation='relu')(x)
+        return Model(input, x)
+    else:
+        input = Input(shape=input_shape)
+        x = Flatten()(input)
+        x = Dense(128, activation='relu')(x)
+        x = Dropout(0.1)(x)
+        x = Dense(128, activation='relu')(x)
+        x = Dropout(0.1)(x)
+        x = Dense(128, activation='relu')(x)
+        return Model(input, x)
 
 
 def compute_accuracy(y_true, y_pred):
@@ -79,72 +155,107 @@ def accuracy(y_true, y_pred):
     return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
 
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-input_shape = x_train.shape[1:]
+# # the data, split between train and test sets
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# x_train = x_train.astype('float32')
+# x_test = x_test.astype('float32')
+# x_train /= 255
+# x_test /= 255
+# input_shape = x_train.shape[1:]
+#
+# # create training+test positive and negative pairs
+# digit_indices = [np.where(y_train == i)[0] for i in range(num_classes)]
+# tr_pairs, tr_y = create_pairs(x_train, digit_indices)
+#
+# digit_indices = [np.where(y_test == i)[0] for i in range(num_classes)]
+# te_pairs, te_y = create_pairs(x_test, digit_indices)
+#
+# %%
+from src.data_loading import get_pairs
+from src.data_loading import get_x_y
+from sklearn.model_selection import train_test_split
 
-# create training+test positive and negative pairs
-digit_indices = [np.where(y_train == i)[0] for i in range(num_classes)]
-tr_pairs, tr_y = create_pairs(x_train, digit_indices)
+pairs, y = get_x_y()
 
-digit_indices = [np.where(y_test == i)[0] for i in range(num_classes)]
-te_pairs, te_y = create_pairs(x_test, digit_indices)
+tr_pairs, te_pairs, tr_y, te_y = train_test_split(pairs,y)
+input_shape = tr_pairs[0][0].shape
 
-# two images of 28x28
-tr_pairs[0].shape
-# are they the same
-tr_y[0]
+# %% network definition
+def get_siamese_model(input_shape):
+    """
+        Model architecture
+    """
+    # Define the tensors for the two input images
+    left_input = Input(input_shape)
+    right_input = Input(input_shape)
+
+    # Convolutional Neural Network
+    model = Sequential()
+    model.add(Conv2D(64, (10,10), activation='relu', input_shape=input_shape,kernel_regularizer=l2(2e-4)))
+    model.add(MaxPooling2D())
+    model.add(Conv2D(128, (7,7), activation='relu', kernel_regularizer=l2(2e-4)))
+    model.add(MaxPooling2D())
+    model.add(Conv2D(128, (4,4), activation='relu', kernel_regularizer=l2(2e-4)))
+    model.add(MaxPooling2D())
+    model.add(Conv2D(256, (4,4), activation='relu', kernel_regularizer=l2(2e-4)))
+    model.add(Flatten())
+    model.add(Dense(4096, activation='sigmoid',kernel_regularizer=l2(1e-3)))
+
+    # Generate the encodings (feature vectors) for the two images
+    encoded_l = model(left_input)
+    encoded_r = model(right_input)
+
+    # Add a customized layer to compute the absolute difference between the encodings
+    L1_layer = Lambda(lambda tensors:K.abs(tensors[0] - tensors[1]))
+    L1_distance = L1_layer([encoded_l, encoded_r])
+
+    # Add a dense layer with a sigmoid unit to generate the similarity score
+    prediction = Dense(1,activation='sigmoid')(L1_distance)
+
+    # Connect the inputs with the outputs
+    siamese_net = Model(inputs=[left_input,right_input],outputs=prediction)
+
+    # return the model
+    return siamese_net
 
 
 # %%
-from src.data_loading import get_pairs
-
-all_pairs = get_pairs()
-
-
-
-len(pairs)
-
-sum(tr_y)
-len(tr_y)-sum(tr_y)
-
-sum(te_y)
-len(te_y)-sum(te_y)
-
-# network definition
-base_network = create_base_network(input_shape)
-
-input_a = Input(shape=input_shape)
-input_b = Input(shape=input_shape)
-
-# because we re-use the same instance `base_network`,
-# the weights of the network
-# will be shared across the two branches
-processed_a = base_network(input_a)
-processed_b = base_network(input_b)
-
-distance = Lambda(euclidean_distance,
-                  output_shape=eucl_dist_output_shape)([processed_a, processed_b])
-
-model = Model([input_a, input_b], distance)
-
-# %% train
+# base_network = create_base_network(input_shape,model_type='alexnet')
+# input_a = Input(shape=input_shape)
+# input_b = Input(shape=input_shape)
+#
+# # because we re-use the same instance `base_network`,
+# # the weights of the network
+# # will be shared across the two branches
+# processed_a = base_network(input_a)
+# processed_b = base_network(input_b)
+#
+# distance = Lambda(euclidean_distance,
+#                   output_shape=eucl_dist_output_shape)([processed_a, processed_b])
+#
+# model = Model([input_a, input_b], distance)
+# model.summary()
+#
+#
+# # %% train
+input_shape = (200,100,1)
+model = get_siamese_model(input_shape)
+model.summary()
 rms = RMSprop()
-model.compile(loss=contrastive_loss, optimizer=rms, metrics=[accuracy])
+model.compile(optimizer='adam')
+# model.compile(loss=contrastive_loss, optimizer=rms, metrics=[accuracy])
 model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
           batch_size=128,
           epochs=epochs,
           validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y))
+
 
 # compute final accuracy on training and test sets
 y_pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
 tr_acc = compute_accuracy(tr_y, y_pred)
 y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
 te_acc = compute_accuracy(te_y, y_pred)
+
 
 print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
 print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
