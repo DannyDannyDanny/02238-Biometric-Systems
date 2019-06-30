6fbb30f06599eaa4296bd7d5a6e38c61f1722047
DannyDannyDanny <s144222@student.dtu.dk>
2019-06-30 18:55:02 +0200

    add: np.squeeze to image-preproc

--- src/data_loading.py
+++ src/data_loading.py
@@ -13,6 +13,8 @@ import glob
 import numpy as np
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
+from skimage import exposure
+
 
 # %% dataprep
 def get_pairs():
@@ -27,19 +29,37 @@ def get_pairs():
 
     for file_train,file_test in zip(train_files,test_files):
         subject_id = int(file_test.split('/')[-2])
+
         img_train = load_img(file_train,color_mode = "grayscale")
-        # arr_train = np.squeeze((img_to_array(img_train)))
-        arr_train = (img_to_array(img_train))
+        arr_train = exposure.equalize_hist(img_to_array(img_train))
+        arr_train = np.squeeze((img_to_array(arr_train)))
+
         img_test = load_img(file_test,color_mode = "grayscale")
-        # arr_test = np.squeeze((img_to_array(img_test)))
-        arr_test = (img_to_array(img_test))
+        arr_test = exposure.equalize_hist(img_to_array(img_test))
+        arr_test = np.squeeze((img_to_array(arr_test)))
+
         pairs.append([arr_test,arr_train])
     return pairs
 
+# %%
+all_pairs = np.array(get_pairs())
+
 def get_x_y():
     all_pairs = np.array(get_pairs())
+
+    # assert all images unique
+    string_imgs = []
+    for pair in all_pairs:
+        string_imgs.append(pair[0].tostring())
+        string_imgs.append(pair[1].tostring())
+    assert len(string_imgs) == len(set(string_imgs))
+    del string_imgs
+
+    # split data for matching pairs and non-matching pairs
     half_len = len(all_pairs)//2
+    # (already) matching pairs
     ymatch = all_pairs[:half_len]
+    # remaining (matching) pairs
     rem = all_pairs[half_len:]
     # generating class shuffled pairs
     no_matches = False
@@ -54,10 +74,9 @@ def get_x_y():
         else:
             print('No matches:',no_matches)
 
-    nmatch = np.array([[rem[a][0],rem[a][1]] for (a,b) in zip(ran1,ran2)])
+    nmatch = np.array([[rem[a][0],rem[b][1]] for (a,b) in zip(ran1,ran2)])
     ymatch = np.array(ymatch)
-    nmatch.shape
-    ymatch.shape
+
 
     # add class
     ymatch = [[1,i] for i in ymatch]
@@ -66,8 +85,40 @@ def get_x_y():
     class_pairs = ymatch + nmatch
     r.shuffle(class_pairs)
 
-    len(class_pairs)
-    class_pairs[0][0]
-    data_y = np.array([[y] for y,x in class_pairs])
+    data_y = np.array([y for y,x in class_pairs])
+    # data_y = np.array([[y] for y,x in class_pairs])
     data_x = np.array([x for y,x in class_pairs])
     return data_x,data_y
+
+all_pairs = get_pairs()
+img_origin_lookup_dict = {}
+for i,(a,b) in enumerate(all_pairs):
+    img_origin_lookup_dict[b.tostring()] = (i,'b')
+    img_origin_lookup_dict[a.tostring()] = (i,'a')
+
+
+pairs, y = get_x_y()
+
+
+pairs.shape
+y.shape
+
+match = 0
+nonmatch = 0
+errors = 0
+# %%
+for pair in pairs:
+    a = img_origin_lookup_dict[pair[0].tostring()]
+    b = img_origin_lookup_dict[pair[1].tostring()]
+    if a[0] == b[0]:
+        match += 1
+    else:
+        nonmatch += 1
+
+    if a[1] == b[1]:
+        errors += 1
+
+
+match
+nonmatch
+errors
