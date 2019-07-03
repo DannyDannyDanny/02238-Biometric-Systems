1051844945168b45ebbaf2f15e9bbb71cf040dbc
DannyDannyDanny <s144222@student.dtu.dk>
2019-07-03 19:55:58 +0200

    add: conv layers and batchnormalization, maxpooling between conv layers

--- src/keras_siamese_MLP1_loop.py
+++ src/keras_siamese_MLP1_loop.py
@@ -5,11 +5,12 @@ import numpy as np
 import random
 from keras.datasets import mnist
 from keras.models import Model
-from keras.layers import Input, Conv2D, Lambda, merge, Dense, Flatten,MaxPooling2D
-from keras.layers import Input, Flatten, Dense, Dropout, Lambda
-from keras.optimizers import RMSprop
+from keras.layers import Input, Conv2D, Lambda, merge, Dense, Flatten, MaxPooling2D
+from keras.layers import Input, Flatten, Dense, Dropout, Lambda, BatchNormalization
+from keras.optimizers import RMSprop, Adam
 from keras import backend as K
 
+from src.data_loading import get_x_y
 num_classes = 2
 epochs = 20
 
@@ -57,15 +58,21 @@ def create_base_network(input_shape):
     '''Base network to be shared (eq. to feature extraction).
     '''
     input = Input(shape=input_shape)
-    # x = Conv2D(96, (7,7),padding='valid',activation='relu',input_shape=input_shape)(input)
-    x = Conv2D(64, kernel_size=(3,3), activation='relu', input_shape=input_shape)(input)
+    x = Conv2D(64, kernel_size=(10,10), activation='relu', input_shape=input_shape)(input)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D()(x)
+    x = Conv2D(32, kernel_size=(5,5), activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D()(x)
+    x = Conv2D(16, kernel_size=(3,3), activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D()(x)
     x = Flatten()(x)
-    x = Dense(512, activation='relu')(x)
-    x = Dropout(0.1)(x)
-    x = Dense(128, activation='relu')(x)
-    x = Dropout(0.1)(x)
-    x = Dense(128, activation='relu')(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(256, activation='relu')(x)
     return Model(input, x)
+
 
 def compute_accuracy(y_true, y_pred):
     '''Compute classification accuracy with a fixed threshold on distances.
@@ -73,12 +80,12 @@ def compute_accuracy(y_true, y_pred):
     pred = y_pred.ravel() < 0.5
     return np.mean(pred == y_true)
 
+
 def accuracy(y_true, y_pred):
     '''Compute classification accuracy with a fixed threshold on distances.
     '''
     return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
 
-from src.data_loading import get_x_y
 
 def get_MLP_model(input_shape):
     # network definition
@@ -96,45 +103,51 @@ def get_MLP_model(input_shape):
     distance = Lambda(euclidean_distance,
                       output_shape=eucl_dist_output_shape)([processed_a, processed_b])
 
+
     model = Model([input_a, input_b], distance)
     return model
 
 
 hist_list = []
-for i in range(1):
-    print('round:',i)
+# for i in range(1):
+# print('round:',i)
 
-    te_pairs, tr_pairs, te_y, tr_y = get_x_y()
-    input_shape = te_pairs[0][0].shape
+te_pairs, tr_pairs, te_y, tr_y = get_x_y()
 
-    model = get_MLP_model(input_shape)
+input_shape = te_pairs[0][0].shape
 
-    rms = RMSprop()
+model = get_MLP_model(input_shape)
 
-    model.summary()
+adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+# %%
+model.summary()
 
+model.compile(loss=contrastive_loss, optimizer=adam, metrics=[accuracy])
 
-    model.compile(loss=contrastive_loss, optimizer=rms, metrics=[accuracy])
-    history = model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
-              batch_size=32,
-              epochs=25,#epochs,
-              validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y),verbose=0)
+# %%
+history = model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
+          batch_size=32,
+          epochs=25,#epochs,
+          validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y),
+          verbose=1)
 
-    history_dict = history.history
+!say finished
+# %%
+history_dict = history.history
 
-    # compute final accuracy on training and test sets
-    y_pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
-    tr_acc = compute_accuracy(tr_y, y_pred)
-    y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
-    te_acc = compute_accuracy(te_y, y_pred)
+# compute final accuracy on training and test sets
+y_pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
+tr_acc = compute_accuracy(tr_y, y_pred)
+y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
+te_acc = compute_accuracy(te_y, y_pred)
 
-    print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
-    print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
+print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
+print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
 
-    acc_dict = {}
-    acc_dict['tr_acc'] = tr_acc
-    acc_dict['te_acc'] = te_acc
-    hist_list.append([acc_dict,history_dict])
+acc_dict = {}
+acc_dict['tr_acc'] = tr_acc
+acc_dict['te_acc'] = te_acc
+hist_list.append([acc_dict,history_dict])
 
 # %%
 import numpy.random as rng
@@ -152,8 +165,6 @@ import time
 import datetime
 ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H:%M:%S')
 ts
-import os
-os.listdir('.')
 # %%
 import matplotlib.pyplot as plt
 plt.plot(history.history['accuracy'])
@@ -164,7 +175,7 @@ plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.savefig('imgs/'+ts+'_accuracy.png')
 plt.show()
-º# %% summarize history for loss
+## %% summarize history for loss
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
