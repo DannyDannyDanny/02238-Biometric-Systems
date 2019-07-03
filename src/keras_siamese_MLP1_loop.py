b349f748cf39fa7922b35eef37250214360d15b8
DannyDannyDanny <s144222@student.dtu.dk>
2019-07-03 12:26:14 +0200

    wip: double conv layer custom network

---
+++ src/keras_siamese_MLP1_loop.py
@@ -0,0 +1,177 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import numpy as np
+
+import random
+from keras.datasets import mnist
+from keras.models import Model
+from keras.layers import Input, Conv2D, Lambda, merge, Dense, Flatten,MaxPooling2D
+from keras.layers import Input, Flatten, Dense, Dropout, Lambda
+from keras.optimizers import RMSprop
+from keras import backend as K
+
+num_classes = 2
+epochs = 20
+
+def euclidean_distance(vects):
+    x, y = vects
+    sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
+    return K.sqrt(K.maximum(sum_square, K.epsilon()))
+
+
+def eucl_dist_output_shape(shapes):
+    shape1, shape2 = shapes
+    return (shape1[0], 1)
+
+
+def contrastive_loss(y_true, y_pred):
+    '''Contrastive loss from Hadsell-et-al.'06
+    http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    '''
+    margin = 1
+    sqaure_pred = K.square(y_pred)
+    margin_square = K.square(K.maximum(margin - y_pred, 0))
+    return K.mean(y_true * sqaure_pred + (1 - y_true) * margin_square)
+
+
+def create_pairs(x, digit_indices):
+    '''Positive and negative pair creation.
+    Alternates between positive and negative pairs.
+    '''
+    pairs = []
+    labels = []
+    n = min([len(digit_indices[d]) for d in range(num_classes)]) - 1
+    for d in range(num_classes):
+        for i in range(n):
+            z1, z2 = digit_indices[d][i], digit_indices[d][i + 1]
+            pairs += [[x[z1], x[z2]]]
+            inc = random.randrange(1, num_classes)
+            dn = (d + inc) % num_classes
+            z1, z2 = digit_indices[d][i], digit_indices[dn][i]
+            pairs += [[x[z1], x[z2]]]
+            labels += [1, 0]
+    return np.array(pairs), np.array(labels)
+
+
+def create_base_network(input_shape):
+    '''Base network to be shared (eq. to feature extraction).
+    '''
+    input = Input(shape=input_shape)
+    # x = Conv2D(96, (7,7),padding='valid',activation='relu',input_shape=input_shape)(input)
+    x = Conv2D(64, kernel_size=(3,3), activation='relu', input_shape=input_shape)(input)
+    x = Flatten()(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.1)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.1)(x)
+    x = Dense(128, activation='relu')(x)
+    return Model(input, x)
+
+def compute_accuracy(y_true, y_pred):
+    '''Compute classification accuracy with a fixed threshold on distances.
+    '''
+    pred = y_pred.ravel() < 0.5
+    return np.mean(pred == y_true)
+
+def accuracy(y_true, y_pred):
+    '''Compute classification accuracy with a fixed threshold on distances.
+    '''
+    return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
+
+from src.data_loading import get_x_y
+
+def get_MLP_model(input_shape):
+    # network definition
+    base_network = create_base_network(input_shape)
+
+    input_a = Input(shape=input_shape)
+    input_b = Input(shape=input_shape)
+
+    # because we re-use the same instance `base_network`,
+    # the weights of the network
+    # will be shared across the two branches
+    processed_a = base_network(input_a)
+    processed_b = base_network(input_b)
+
+    distance = Lambda(euclidean_distance,
+                      output_shape=eucl_dist_output_shape)([processed_a, processed_b])
+
+    model = Model([input_a, input_b], distance)
+    return model
+
+
+hist_list = []
+for i in range(1):
+    print('round:',i)
+
+    te_pairs, tr_pairs, te_y, tr_y = get_x_y()
+    input_shape = te_pairs[0][0].shape
+
+    model = get_MLP_model(input_shape)
+
+    rms = RMSprop()
+
+    model.summary()
+
+
+    model.compile(loss=contrastive_loss, optimizer=rms, metrics=[accuracy])
+    history = model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
+              batch_size=32,
+              epochs=25,#epochs,
+              validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y),verbose=0)
+
+    history_dict = history.history
+
+    # compute final accuracy on training and test sets
+    y_pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
+    tr_acc = compute_accuracy(tr_y, y_pred)
+    y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
+    te_acc = compute_accuracy(te_y, y_pred)
+
+    print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
+    print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
+
+    acc_dict = {}
+    acc_dict['tr_acc'] = tr_acc
+    acc_dict['te_acc'] = te_acc
+    hist_list.append([acc_dict,history_dict])
+
+# %%
+import numpy.random as rng
+from src.data_loading import data_to_file
+data = hist_list
+this_rng = str(rng.random())[2:6]
+data_to_file('MLPmulti_2'+this_rng,data)
+
+# %%
+for acc,hist in hist_list:
+    print(acc)
+
+# %%
+import time
+import datetime
+ts = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H:%M:%S')
+ts
+import os
+os.listdir('.')
+# %%
+import matplotlib.pyplot as plt
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('imgs/'+ts+'_accuracy.png')
+plt.show()
+º# %% summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('imgs/'+ts+'_loss.png')
+plt.show()
+# %%
+!say attention! training complete!
