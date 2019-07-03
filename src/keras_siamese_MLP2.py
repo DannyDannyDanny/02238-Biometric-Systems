24d268a12f138bebadb78d35899e79025506623b
DannyDannyDanny <s144222@student.dtu.dk>
2019-07-03 12:25:19 +0200

    wip: siamese convnet attempt 1

---
+++ src/keras_siamese_MLP2.py
@@ -0,0 +1,73 @@
+from keras.layers import Input, Conv2D, Lambda, merge, Dense, Flatten,MaxPooling2D
+from keras.models import Model, Sequential
+from keras.regularizers import l2
+from keras import backend as K
+from keras.optimizers import SGD,Adam
+from keras.losses import binary_crossentropy
+import numpy.random as rng
+import numpy as np
+import os
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.utils import shuffle
+%matplotlib inline
+def W_init(shape,name=None):
+    """Initialize weights as in paper"""
+    values = rng.normal(loc=0,scale=1e-2,size=shape)
+    return K.variable(values,name=name)
+#//TODO: figure out how to initialize layer biases in keras.
+def b_init(shape,name=None):
+    """Initialize bias as in paper"""
+    values=rng.normal(loc=0.5,scale=1e-2,size=shape)
+    return K.variable(values,name=name)
+
+from src.data_loading import get_x_y
+te_pairs, tr_pairs, te_y, tr_y = get_x_y()
+
+# input_shape = (105, 105, 1)
+input_shape = te_pairs[0][0].shape
+print(input_shape)
+left_input = Input(input_shape)
+right_input = Input(input_shape)
+#build convnet to use in each siamese 'leg'
+convnet = Sequential()
+convnet.add(Conv2D(64,(10,10),activation='relu',input_shape=input_shape,
+                   kernel_initializer=W_init,kernel_regularizer=l2(2e-4)))
+convnet.add(MaxPooling2D())
+convnet.add(Conv2D(128,(7,7),activation='relu',
+                   kernel_regularizer=l2(2e-4),kernel_initializer=W_init,bias_initializer=b_init))
+convnet.add(MaxPooling2D())
+convnet.add(Conv2D(128,(4,4),activation='relu',kernel_initializer=W_init,kernel_regularizer=l2(2e-4),bias_initializer=b_init))
+convnet.add(MaxPooling2D())
+convnet.add(Conv2D(256,(4,4),activation='relu',kernel_initializer=W_init,kernel_regularizer=l2(2e-4),bias_initializer=b_init))
+convnet.add(Flatten())
+convnet.add(Dense(4096,activation="sigmoid",kernel_regularizer=l2(1e-3),kernel_initializer=W_init,bias_initializer=b_init))
+
+#call the convnet Sequential model on each of the input tensors so params will be shared
+encoded_l = convnet(left_input)
+encoded_r = convnet(right_input)
+#layer to merge two encoded inputs with the l1 distance between them
+L1_layer = Lambda(lambda tensors:K.abs(tensors[0] - tensors[1]))
+#call this layer on list of two input tensors.
+L1_distance = L1_layer([encoded_l, encoded_r])
+prediction = Dense(1,activation='sigmoid',bias_initializer=b_init)(L1_distance)
+siamese_net = Model(inputs=[left_input,right_input],outputs=prediction)
+
+optimizer = Adam(0.00006)
+#//TODO: get layerwise learning rates and momentum annealing scheme described in paperworking
+siamese_net.compile(loss="binary_crossentropy",optimizer=optimizer)
+
+# %%
+siamese_net.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
+          batch_size=128,
+          epochs=40,#epochs,
+          validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y),verbose=1)
+# compute final accuracy on training and test sets
+y_pred = siamese_net.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
+tr_acc = compute_accuracy(tr_y, y_pred)
+y_pred = siamese_net.predict([te_pairs[:, 0], te_pairs[:, 1]])
+te_acc = compute_accuracy(te_y, y_pred)
+print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
+print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
+!say attention! attention! training has completed! yeet yeet yeet yeet!
